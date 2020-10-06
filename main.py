e645211874d5f2b4626740b3c267750db09ea968
Daniel Dermont <ddermont13@gmail.com>
2020-10-06 19:27:56 -0400

    Working bash script; changes to tp.sh and main.py

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import math
 import sys
 import time
 import csv
+import os
 
 from brute_force import execute_brute_force
 from DpR import execute_DpR
 from utils import GRID_SIZE, getPointsfromFile
 
 ALGO = sys.argv[1] # Algo à utiliser DPR ou BF
-FILE = sys.argv[2] # Filepath
+FILE = sys.argv[2] # Filepath 
+if (len(sys.argv) < 4):
+    MARKER = ""
+else:
+    MARKER = sys.argv[3] # -p or -t
 
+    
 
 '''
 Un point est représenté par un tuple (position_x, position_y)
@@ -30,24 +36,37 @@ compatible avec ce code (par exemple l'utilisation de flag -e, -a, (p et -t)).
 --------------------------------------------------------------------
  '''
 
-def main(algo, filepath):
+def main(algo, filepath, marker):
+
+    cwd = os.getcwd()
+    filepath = cwd + filepath
     POINTS = getPointsfromFile(filepath)
     sorted_points_x = sorted(POINTS, key=lambda x: x[0])
     sorted_points_y = sorted(POINTS, key=lambda x: x[1])
     
-    if algo == "BF":
+    if algo == "brute":
         # Exécuter l'algorithme force brute
         time_BF, dist_BF, points_BF = execute_brute_force(sorted_points_x)
-        print("Time : ", time_BF)
-        print("Minimum distance : ", dist_BF)
-        print("Solution Points: ", points_BF)
+        if (marker == 'p'):
+            print("Minimum distance : ", dist_BF)
+        elif (marker == 't'):
+            print("Time : ", time_BF)
+        else:
+            print("Time : ", time_BF)
+            print("Minimum distance : ", dist_BF)
+            print("Solution Points: ", points_BF)
     
-    elif algo == "DPR":
+    elif algo == "recursif":
         # Exécuter l'algorithme Diviser pour régner
         SEUIL_DPR = 20
         time_DPR, dist_DPR, points_DPR = execute_DpR(sorted_points_x, sorted_points_y, SEUIL_DPR)
-        print("Time : ", time_DPR)
-        print("Minimum distance : ", dist_DPR)
-        print("Solution Points: ", points_DPR)
+        if (marker == 'p'):
+            print("Minimum distance : ", dist_BF)
+        elif (marker == 't'):
+            print("Time : ", time_BF)
+        else:
+            print("Time : ", time_BF)
+            print("Minimum distance : ", dist_BF)
+            print("Solution Points: ", points_BF)
         
-main(ALGO, FILE)
+main(ALGO, FILE, MARKER)
--- tp.sh
+++ tp.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-
-./executable "$@"
+while getopts a:e:pt flag
+do
+    case "${flag}" in
+        a) algo=${OPTARG};;
+        e) ext=${OPTARG};;
+        p) marker=p;;
+        t) marker=t;;      
+    esac
+done
+python3 main.py $algo $ext $marker
