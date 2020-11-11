b5af0b7cc759ad8a3b5fcfeb45f4d55372ba9c97
Daniel Dermont <ddermont13@gmail.com>
2020-11-10 20:15:22 -0500

    Updated parse to create instead a 2d numpy array blocks = 3n x {h | l | p | ID | Surface Area }

---
+++ TP2/parse_2.py
@@ -0,0 +1,49 @@
+import sys
+import os
+
+import numpy as np
+
+FILEPATH = sys.argv[1]
+
+
+def createBlocks(path):
+	# path is a string containing absolute path from 
+	# Isolate the sample size from the path string
+	part = path.split("b")
+	n_strings = part.pop().split("_")
+	n = int(n_strings[0])
+	# numLines represents expected length of text file
+	numLines = 3*n
+
+	# Instantiate blocks array with:
+	# B = numLines x { h | l | p | ID | SurfaceArea }
+	blocks = np.zeros((numLines,5),dtype=int)
+	with open(path,"r") as infile:
+		for i in range(numLines):
+			# store h,l,p as first three entries
+			hlp = infile.readline().split(" ")
+			blocks[i,0], blocks[i,1], blocks[i,2] = int(hlp[0]),int(hlp[1]),int(hlp[2])
+			# fourth entry is ID (numLines floor divided by 3)
+			blocks[i,3] = int(i//3)
+			#fifth entry is Surface Area
+			blocks[i,4] = (blocks[i][1]*blocks[i][2])
+	return blocks
+def printBlocks(Blocks):
+	for i in range(Blocks.shape[0]):
+		print("Dimensions of block ", i, " for each orientation:")
+		print ("h: ", Blocks[i,0], ", l: ", Blocks[i,1], ", p: ", Blocks[i,2],", ID: ", Blocks[i,3],", Surface Area: ", Blocks[i,4])
+
+#def sortBySA(unsorted):
+#	
+#	return sorted
+
+def main(filepath):
+
+	Blocks = createBlocks(filepath)
+	printBlocks(Blocks)
+	
+
+main(FILEPATH)
+	
+	
+		
