7eef7ab292df9ba3276e7d40b799b7856bcf062a
Daniel Dermont <ddermont13@gmail.com>
2020-11-12 16:44:28 -0500

    added tp script with (hopefully) lf line endings. confroms to usage in TP2.odt

--- TP2/tp.sh
+++ TP2/tp.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-
-./executable "$@"
+while getopts a:e:pt flag
+do
+    case "${flag}" in
+        a) algo=${OPTARG};;
+        e) ext=${OPTARG};;
+        p) marker1='p';;
+        t) marker2='t';;      
+    esac
+done
+python3 main.py $algo $ext $marker1 $marker2
