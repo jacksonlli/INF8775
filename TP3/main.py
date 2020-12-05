b1c105d666849522e6f13c0ea64a2197196f10ea
jacksonlli <59549394+jacksonlli@users.noreply.github.com>
2020-12-04 19:33:59 -0500

    solution loop

--- TP3/main.py
+++ TP3/main.py
@@ -4,38 +4,69 @@ import time
 from params import RANDMUNIRATIO
 import sys
 import random
+from copy import deepcopy
+
+sys.setrecursionlimit(10000)  
 
 FILEPATH = sys.argv[1]
-x, y, data = createMuniField(FILEPATH)
+x, y, availableMuniOriginal = createMuniField(FILEPATH)
 
-start = time.time()
 
-m = random.randint(round(x*y/20), round(x*y/2))
-k = 20#round(x*y/m)
+m = int(sys.argv[2])
+k = round(x*y/m)
 randMuniNum = round(RANDMUNIRATIO*k)#within a conscript
 
+bestScore = -999999999
+bestSolution = []
+max_dist = math.ceil(x*y/(2*m))
 
-initMuni, max_dist = random_init(data, x, y, m)
-#print("Full field:", data)
-#print(init_candidates)
-#print("Proposed chosen municipality: ", initMuni)
-#print("Sub-array input to random_expand: ", subC)
-failed = True
-while(failed):
-	newConscrip, failed, visitedList = random_expand(data, initMuni, max_dist, x, y, randMuniNum, 100)
-#print("Initial Conscription", newConscrip)
-isValid = False
-while(not isValid):
-	backTrackCandidateList = initCandidateList(data, newConscrip, max_dist, x, y)
-	#print("Valid candidates", backTrackCandidateList)
-	finalConscrip, finalScore, isValid = backtracking(backTrackCandidateList, newConscrip, max_dist, k, randMuniNum+1, getCurrentSum(newConscrip), None, None, visitedList, False)
-	
-print("Final Conscription ", finalConscrip)
-print("Final Score ", finalScore)
-		
-		
+solutionScore = 0
+solutionList = []
+start = time.time()
+availableMunicipalities = []
+initMuni = None
+newConscrip = []
+isExpandFailed = True
+visitedList = []
+backTrackCandidateList =[]
+finalConscrip = []
+finalScore = None
+isBTValid = False
 
+while(True):
+	availableMunicipalities = availableMuniOriginal.copy()
+	solutionScore = 0
+	solutionList = []
+	start = time.time()
+	for i in range(m):
+		
+		initMuni = availableMunicipalities.pop(0)#random_init(availableMunicipalities, x, y, m)
+		newConscrip, isExpandFailed, visitedList = random_expand(availableMunicipalities, initMuni, max_dist, x, y, randMuniNum, 100)
+		if isExpandFailed:
+			break
+		backTrackCandidateList = initCandidateList(availableMunicipalities, newConscrip, max_dist, x, y)
+		finalConscrip, finalScore, isBTValid = backtracking(backTrackCandidateList, newConscrip, max_dist, k, randMuniNum+1, getCurrentSum(newConscrip), None, None, visitedList, False)
+		availableMunicipalities = updateAvailableMunicipality(finalConscrip, availableMunicipalities)
+		if not isBTValid:
+			break
+		if finalScore > 50:
+			solutionScore += 1
+		solutionList.append(finalConscrip)	
+	if isExpandFailed or not isBTValid:
+		#print("fail")
+		pass
+	elif solutionScore > bestScore:
+		bestScore = solutionScore
+		bestSolution = solutionList
+		print(bestSolution)
+		end = time.time()
+		print("Time ", end - start)
+		#printSolution(bestSolution)
+		start = time.time()
+	else:
+		print("iteration done")
+		print("Time ", end - start)
+		start = time.time()
+	end = time.time()
 	
-end = time.time()
-print("Time ", end - start)
 	
--- TP3/util_init.py
+++ TP3/util_init.py
@@ -10,15 +10,13 @@ def createMuniField(path):	# creates a 2d-list of municipalities [y,x]
 		x = int(xy[0])
 		y = int(xy[1])
 		field = [[0] * x for i in range(y)]
-		formattedData = []
-		for i in range(x):
-			formattedData.append([])
+		availableMuni = []
 		for j in range(y):
 			row = infile.readline().split()
 			for k in range(x):
 				field[j][k] = int(row[k])
-				formattedData[k].append((k, j, field[j][k]))
-	return x, y, formattedData
+				availableMuni.append((k, j, field[j][k]))
+	return x, y, availableMuni
 
 def manDist(xy1, xy2):
 	return (abs(xy1[0] - xy2[0]) + abs(xy1[1]-xy2[1]))
@@ -30,14 +28,11 @@ def isLegalConscrip(conscrip, newMuni, max_dist):#O(n)
 			return False
 	return True
 
-def initCandidateList(data, conscrip, max_dist, x, y):
-	x0, y0, _ = conscrip[0]
-	xMax, xMin, yMax, yMin = subArrayBounds(x0, y0, max_dist, x, y)
+def initCandidateList(availMuniList, conscrip, max_dist, x, y):
 	candidateList = []
-	for i in range(xMin, xMax + 1):
-		for j in range(yMin, yMax + 1):
-			if isLegalConscrip(conscrip, data[i][j], max_dist):
-				candidateList.append(data[i][j])
+	for muni in availMuniList:
+		if isLegalConscrip(conscrip, muni, max_dist):
+			candidateList.append(muni)
 	return candidateList
 
 
@@ -56,41 +51,39 @@ def subArrayBounds(x0, y0, max_dist, x, y):
 		yMin = 0
 	return xMax, xMin, yMax, yMin
 
-def random_init(data, x, y, m):
+def random_init(availMuni, x, y, m):
 	# buffer candidates input and initialize n and max_dist
+	choose = random.randint(0, len(availMuni) - 1)
 	n = x*y
 	max_dist = math.ceil(n/(2*m))
-	print("Max Dist ", max_dist)
-	# choose a random candidate
-	chooseX = random.randint(0, (x - 1))
-	chooseY = random.randint(0, (y - 1))
-	# remove the choice and save it for return (this is the first muni in the proposed conscrip)
-	initMuni = data[chooseX][chooseY]	
-	return initMuni, max_dist
+	#print("Max Dist ", max_dist)
+	chosenMuni = availMuni.pop(choose)
+	return chosenMuni
+		
 	
-def random_expand(data, initMuni, max_dist, x, y, k, iter_lim):
+def random_expand(availMuni, initMuni, max_dist, x, y, k, iter_lim):
 	# add k more random guesses, evaluate if they are legal to form conscription
 	# if there do not exist k candidates or iter_lim is reached, return failure
 	initConscrip = []
 	initConscrip.append(initMuni)
 	x0, y0, _ = initMuni
-	xMax, xMin, yMax, yMin = subArrayBounds(x0, y0, max_dist, x, y)
 	failed = True
-	print("Begin Expansion Loop...")
+	#print("Begin Expansion Loop...")
 	for i in range(iter_lim):
 		currentConscrip = initConscrip.copy()
 		visitedList = [[False for y in range(y)] for i in range(x)]
 		for j in range(k):
 			for l in range(round(iter_lim/2)):#attempt to add a muni to the conscrip with limited attempts
 				# choose one candidate at random
-				chooseX = random.randint(xMin, xMax)
-				chooseY = random.randint(yMin, yMax)
+				choose = random.randint(0, len(availMuni) - 1)
+				chosenMuni = availMuni[choose]
 				#print("Proposed addition", data[chooseX][chooseY])
 				# check if the proposed conscription is legal
-				if(isLegalConscrip(currentConscrip, data[chooseX][chooseY], max_dist)):
+				if(isLegalConscrip(currentConscrip, chosenMuni, max_dist)):
 					failed = False
-					currentConscrip.append(data[chooseX][chooseY])
-					visitedList[chooseX][chooseY] = True
+					currentConscrip.append(chosenMuni)
+					visitedList[chosenMuni[0]][chosenMuni[1]] = True
+					availMuni.pop(choose)
 					break
 				else:
 					failed = True
@@ -98,12 +91,15 @@ def random_expand(data, initMuni, max_dist, x, y, k, iter_lim):
 				break
 		if not failed: #if successfully added all k muni, return the conscrip
 			visitedList[x0][y0] = True
-			print("Expansion Success!")
+			#print("Expansion Success!")
 			return currentConscrip, failed, visitedList
 	return initConscrip, failed, []
 
+def updateAvailableMunicipality(conscrip, availMuniList):
+	if conscrip:
+		return [x for x in availMuniList if x not in conscrip]
+	else:
+		return availMuniList
 
-
-	
 #print(makeCandidates(field, x, y))
 #print(field)
