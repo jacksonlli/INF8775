75ec6ae749fbb3d2c4e913096ac6aafa67900e2b
jacksonlli <59549394+jacksonlli@users.noreply.github.com>
2020-12-11 23:50:43 -0500

    clean

--- TP3/main.py
+++ TP3/main.py
@@ -1,41 +1,21 @@
-from util_init import *
-from backtracking import backtracking, getCurrentSum
+from utils import *
 import time
-from params import RANDMUNIRATIO
 import sys
-import random
 
 FILEPATH = sys.argv[1]
+M = sys.argv[2]
+m = int(M)
+MARKER1 = ""
+if (len(sys.argv) >= 4):
+    MARKER1 = sys.argv[3]
+	
 x, y, data = createMuniField(FILEPATH)
 
-start = time.time()
-
-m = random.randint(round(x*y/20), round(x*y/2))
-k = 20#round(x*y/m)
-randMuniNum = round(RANDMUNIRATIO*k)#within a conscript
-
-
-initMuni, max_dist = random_init(data, x, y, m)
-#print("Full field:", data)
-#print(init_candidates)
-#print("Proposed chosen municipality: ", initMuni)
-#print("Sub-array input to random_expand: ", subC)
-failed = True
-while(failed):
-	newConscrip, failed, visitedList = random_expand(data, initMuni, max_dist, x, y, randMuniNum, 100)
-#print("Initial Conscription", newConscrip)
-isValid = False
-while(not isValid):
-	backTrackCandidateList = initCandidateList(data, newConscrip, max_dist, x, y)
-	#print("Valid candidates", backTrackCandidateList)
-	finalConscrip, finalScore, isValid = backtracking(backTrackCandidateList, newConscrip, max_dist, k, randMuniNum+1, getCurrentSum(newConscrip), None, None, visitedList, False)
-	
-print("Final Conscription ", finalConscrip)
-print("Final Score ", finalScore)
-		
-		
-
-	
-end = time.time()
-print("Time ", end - start)
-	
+#start = time.time()
+isValid, greedyOut, conscripList = getInitConscriptions(x, y, data, m)
+#end = time.time()
+if MARKER1 == 'p':
+	printDists(x, y, greedyOut)
+else:
+	print(score(greedyOut))
+#print("Time ", end - start)
--- TP3/params.py
+++
@@ -1,2 +0,0 @@
-TOL = 0.1 #increase for more speed, decrease for accuracy (value has to be > 0 and < 50, preferably <5)
-RANDMUNIRATIO = 1/3 #increase for speed, decrease for accuracy
